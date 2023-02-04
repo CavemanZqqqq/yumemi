91fc46e07f953e31fd7a00a2de17eb82bd65b74f
JIAQI ZOU <caishen97888@gmail.com>
2023-02-04 13:28:16 +0900

    day3

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
-//创建仓库
+
 import {applyMiddleware, createStore} from 'redux'
 
-//导出reducers
+
 import rootReducer from './reducers'
 
-//引用日志
-import logger from 'redux-logger'
 
-//导出仓库
+//import logger from 'redux-logger'
+
+
 export default createStore(rootReducer,applyMiddleware(logger));
